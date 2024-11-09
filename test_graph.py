7efe5676599ff10f7ecee6d42f3f5285d7720c71
Abdu Mohammed Kebede <118827226+Abdu1964@users.noreply.github.com>
2024-11-10 01:21:35 +0300

    Update test_graph.py

--- test_graph.py
+++ test_graph.py
@@ -1,4 +1,3 @@
-# test_graph.py
 import pytest
 from main import Graph  # Import your Graph class
 
@@ -12,5 +11,4 @@ def test_dfs():
     graph = Graph()
     graph.build_graph_from_file('cities.txt')
     path = graph.dfs("Seattle", "Miami")  # Check if DFS finds a valid path
-    assert path is not None
-
+    assert path is not None  # Ensure a path exists
